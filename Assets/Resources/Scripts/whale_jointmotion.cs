d7fa325f23dc226315500db1f8e51bae87706ee4
kitekii <tim720719@163.com>
2019-03-16 18:27:56 +0800

    fix whale model and add script to joint bones

---
+++ Assets/Resources/Scripts/whale_jointmotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whale_jointmotion : MonoBehaviour {
+
+    public GameObject parentJoint;
+
+    public float velocity = 1;
+	
+    private Vector3 parentToThis;
+
+    // Use this for initialization
+    void Start () {
+        parentToThis = -parentJoint.transform.position + this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Vector3.Lerp(this.transform.position, parentJoint.transform.position + parentToThis, Time.deltaTime * velocity);
+    }
+}
