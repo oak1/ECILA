b4b63e320bfd2edb48039745fb62f034419d9e0d
King.S <2586534184@qq.com>
2019-03-18 11:26:24 +0800

    Generator Wall

--- Assets/env_wallGenerator.cs
+++ Assets/env_wallGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class env_wallGenerator : MonoBehaviour {
 
+    public Vector3 initialPosition;
+    public GameObject initialRock;
+
     public GameObject Rock_Wall_2C_01;
     public GameObject Rock_Wall_2C_02;
     public GameObject Rock_Wall_2C_03;
@@ -13,24 +16,70 @@ public class env_wallGenerator : MonoBehaviour {
     public GameObject Rock_Wall_2C_07;
     public GameObject FollowPoint;
 
-    private Vector3 currentPosition;
-    private GameObject lastRock;
+    private Transform currentPosition;        //鲸鱼头部当前的坐标
+    private Vector3 lastPosition;           //上一块生成的Rock的坐标
+    private Transform nextPostion;            //下一块石头的坐标
+    private GameObject lastRock;            //上一块生成的Rock
 
 
     // Use this for initialization
     void Start () {
-        currentPosition = FollowPoint.transform.forward;
-        
-	}
+        lastRock = initialRock;
+        lastPosition = initialPosition;
+        GameObject gameObject = GameObject.Instantiate(Rock_Wall_2C_01, initialPosition,initialRock.transform.rotation) as GameObject;
+        //currentPosition = FollowPoint.transform;
+        //lastPosition.position = initialPosition;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
+        //GameObject gameObject = GameObject.Instantiate(Rock_Wall_2C_01, lastPosition, true) as GameObject;
+
         //根据前一个Rock类型判断下一个Rock的中心坐标
-        
+
+        if(lastRock != null)
+        {
+            lastPosition = lastRock.transform.position;
+        }
+
+        System.Random ran = new System.Random();
+        int i = ran.Next(1, 8);
+        GameObject nextRock;
+        if (i == 1)
+        {
+            nextRock = GameObject.Instantiate(Rock_Wall_2C_01, lastPosition + GetRockHalfLength(Rock_Wall_2C_01) + GetRockHalfLength(lastRock), initialRock.transform.rotation);
+        }
+        else if(i == 2)
+        {
+            nextRock = GameObject.Instantiate(Rock_Wall_2C_02, lastPosition + GetRockHalfLength(Rock_Wall_2C_02) + GetRockHalfLength(lastRock), initialRock.transform.rotation);
+        }
+        else if (i == 3)
+        {
+            nextRock = GameObject.Instantiate(Rock_Wall_2C_03, lastPosition + GetRockHalfLength(Rock_Wall_2C_03) + GetRockHalfLength(lastRock), initialRock.transform.rotation);
+        }
+        else if (i == 4)
+        {
+            nextRock = GameObject.Instantiate(Rock_Wall_2C_04, lastPosition + GetRockHalfLength(Rock_Wall_2C_04) + GetRockHalfLength(lastRock), initialRock.transform.rotation);
+        }
+        else if (i == 5)
+        {
+            nextRock = GameObject.Instantiate(Rock_Wall_2C_05, lastPosition + GetRockHalfLength(Rock_Wall_2C_05) + GetRockHalfLength(lastRock), initialRock.transform.rotation);
+        }
+        else if (i == 6)
+        {
+            nextRock = GameObject.Instantiate(Rock_Wall_2C_06, lastPosition + GetRockHalfLength(Rock_Wall_2C_06) + GetRockHalfLength(lastRock), initialRock.transform.rotation);
+        }
+        else
+        {
+            nextRock = GameObject.Instantiate(Rock_Wall_2C_07, lastPosition + GetRockHalfLength(Rock_Wall_2C_07) + GetRockHalfLength(lastRock), initialRock.transform.rotation);
+        }
+        lastRock = nextRock;
 	}
 
     //获取各种Rock的半长
+
     Vector3 GetRockHalfLength(GameObject Rock)
     {
         if (Rock == Rock_Wall_2C_01)
@@ -66,4 +115,5 @@ public class env_wallGenerator : MonoBehaviour {
             return new Vector3(0, 0, 0);
         }
     }
+
 }
